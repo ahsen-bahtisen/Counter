2b23307450efcf8ae0e74d7d67c909a3081682ae
ozcgokberk <ozcangokberk06@gmail.com>
2022-11-13 11:49:11 +0300

    -viewconttoller düzenlendi.

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLeftLabel: UILabel!
+    @IBOutlet var highScoreLabel: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var scoreButton: UIButton!
+    
+    var counter: Int = 15
+    var score: Int = 0
+    var highScore: Int = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.setTitle("Start", for: .normal)
+        timeLeftLabel.text = ("\(counter)")
+        scoreButton.isHidden = true
+
     }
-
-
+    
+    @IBAction func startButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        startButton.isHidden = true
+        scoreButton.isHidden = false
+    }
+    
+    @IBAction func scoreButton(_ sender: Any) {
+        score += 1
+        scoreButton.setTitle("\(score)", for: .normal)
+        
+    }
+    
+    @objc func timerFunction(){
+        timeLeftLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == -1 {
+            timeLeftLabel.text = "Time's Over."
+            if self.score > self.highScore {
+                self.highScore = self.score
+//                UserDefaults.standard.set(self.highScore, forKey: "highscore")
+                
+            }
+        }
+    }
 }
-
